4ff8d2e240faa83134a9a6a109a0ea755f048b05
Alpha <alphagma@gmail.com>
2024-09-10 08:25:26 -0400

    v1.1: alpine psql client

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,10 @@
-docker compose build
-docker compose up -d database
-docker compose run tests
-return_code=$?
-docker compose down
-echo $return_code
+run_tests() {
+    docker compose build
+    docker compose up -d database
+    docker compose run tests
+    return_code=$?
+    docker compose down
+    return $return_code
+}
+
+run_tests
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,3 @@
-wait-for database:5432 --timeout=60 -- echo "Database is up."
+#!/bin/sh
+./wait-for.sh database:5432 --timeout=60 -- echo "Database is up."
 PGPASSWORD=postgres pg_prove -U postgres -h database -r --ext .sql ./
